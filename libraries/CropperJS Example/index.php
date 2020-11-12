b9663ac611e53a4ea84e5e7b5b3046a5351bfdff
unknown <nigarofe@gmail.com>
2020-11-12 07:35:53 -0300

    Update Example

--- libraries/CropperJS Example/index.php
+++ libraries/CropperJS Example/index.php
@@ -7,13 +7,14 @@
 
 <head>
     <title>Crop Image Before Upload using CropperJS with PHP</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="https://unpkg.com/dropzone/dist/dropzone.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+
     <link href="https://unpkg.com/cropperjs/dist/cropper.css" rel="stylesheet" />
-    <script src="https://unpkg.com/dropzone"></script>
-    <script src="https://unpkg.com/cropperjs"></script>
+    <script src="https://unpkg.com/cropperjs/dist/cropper.js"></script>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 
     <style>
         .image_area {
@@ -37,18 +38,6 @@
             max-width: 1000px !important;
         }
 
-        .overlay {
-            position: absolute;
-            bottom: 10px;
-            left: 0;
-            right: 0;
-            background-color: rgba(255, 255, 255, 0.5);
-            overflow: hidden;
-            height: 0;
-            transition: .5s ease;
-            width: 100%;
-        }
-
         .image_area:hover .overlay {
             height: 50%;
             cursor: pointer;
@@ -69,54 +58,47 @@
 </head>
 
 <body>
-    <div class="container" align="center">
-        <br />
-        <h3 align="center">Crop Image Before Upload using CropperJS with PHP</h3>
-        <br />
-        <div class="row">
-            <div class="col-md-4">&nbsp;</div>
-            <div class="col-md-4">
-                <div class="image_area">
-                    <form action="upload.php" method="post">
-                        <label for="upload_image">
-                            <img src="upload/user.png" id="uploaded_image" class="img-responsive img-circle" />
-                            <div class="overlay">
-                                <div class="text">Click to Change Profile Image</div>
-                            </div>
-                            <input type="file" name="image" class="image" id="upload_image" style="display:none" />
-                        </label>
-                    </form>
-                </div>
+    <h3 class="text-center my-5">Crop Image Before Upload using CropperJS with PHP</h3>
+    <div class="row justify-content-around">
+        <div class="col-md-4 mt-5">
+            <div class="image_area">
+                <form action="upload.php" method="post">
+                    <label for="upload_image">
+                        <img src="upload/user.png" id="uploaded_image" class="img-responsive img-circle" />
+                        <input type="file" name="image" class="image" id="upload_image" />
+                    </label>
+                </form>
             </div>
-            <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
-                <div class="modal-dialog modal-lg" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title">Crop Image Before Upload</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">×</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            <div class="img-container">
-                                <div class="row">
-                                    <div class="col-md-8">
-                                        <img src="" id="sample_image" />
-                                    </div>
-                                    <div class="col-md-4">
-                                        <div class="preview"></div>
-                                    </div>
+        </div>
+        <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Crop Image Before Upload</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="img-container">
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <img src="" id="sample_image" />
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="preview"></div>
                                 </div>
                             </div>
                         </div>
-                        <div class="modal-footer">
-                            <button type="button" id="crop" class="btn btn-primary">Crop</button>
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" id="crop" class="btn btn-primary">Crop</button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
 </body>
 
 </html>
