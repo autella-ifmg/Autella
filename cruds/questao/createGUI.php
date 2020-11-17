917c50caf7d5d52654805aa5aa816cb3ff95d6cd
jose-roberto <juserobertu@gmail.com>
2020-11-16 22:44:22 -0300

    Travar o selectAC enquanto o selectQA ainda não tiver sido preenchido

--- cruds/questao/createGUI.php
+++ cruds/questao/createGUI.php
@@ -21,110 +21,77 @@
     <!--Inclui a navbar-->
     <?php require_once '../../views/navbar.php'; ?>
 
-    <!--Primeiro título-->
-    <h1 class="text-center">Autella | Crie sua questão</h1>
-
     <!--Formulário-->
-    <form id="questionsForm" action="createSQL.php" method="post">
-        <!--Seção dos selects-->
-        <section>
-            <!--Segundo título-->
-            <h1 class="text-left">Informações gerais</h1>
-
-            <!--Select das matérias-->
-            <div class="form-group">
-                <label for="subjects">Matéria:</label>
-                <select name="subjects" id="subjects" class="dropdown-toggle" autofocus required>
-                    <option></option>
-                    <?php
-                    subjectNamesToDropdownItems($id_discipline);
-                    ?>
-                </select>
-            </div>
-
-            <!--Select da dificuldade-->
-            <div class="form-group">
-                <label for="dificulty">Dificuldade:</label>
-                <select name="dificulty" id="dificulty" class="btn btn-primary dropdown-toggle" required>
-                    <option></option>
-                    <option value="1">Fácil</option>
-                    <option value="2">Médio</option>
-                    <option value="3">Difícil</option>
-                </select>
-            </div>
-
-            <!--Select do número de alternativas-->
-            <div class="form-group">
-                <label for="alternativesQuant">Número de alternativas:</label>
-                <select name="alternativesQuant" id="alternativesQuant" class="btn btn-primary dropdown-toggle" onchange="updateCorrectAnswerField_AlternativesField()" required>
-                    <option></option>
-                    <option value=4>4</option>
-                    <option value=5>5</option>
-                </select>
-            </div>
-
-            <!--Select da alternativa correta-->
-            <div class="form-group">
-                <label for="correctAnswer">Alternativa correta:</label>
-                <select name="correctAnswer" id="correctAnswer" class="btn btn-primary dropdown-toggle" required></select>
-            </div>
-        </section>
-
-        <!--Seção dos enunciados-->
-        <section>
-            <!--Terceiro título-->
-            <h1 class="text-left">Enunciados</h1>
-
-            <!--Enunciado da questão-->
-            <div>
-                <!--Barra de ferramentas-->
-                <div name="toolbar" id="toolbar-container"></div>
-                <!--Campo de texto-->
-                <div name="editor" id="editor" style="height: 20rem; border: 1px solid black;"></div>
-            </div>
-
-            <!--Enunciados das alternativas-->
-            <div id="alternatives_container" class="d-flex flex-column"></div>
-        </section>
-
-        <!--Seção dos botões-->
-        <section>
-            <!--Botão para cancelar-->
-            <a href="../../views/home.php" type="button" class="btn btn-danger ml-0 mr-1">Cancelar</a>
-            <!--Botão para listar questões-->
-            <a href="updateGUI.php" type="button" class="btn btn-primary ml-1 mr-0 ml-0 mr-1">Visualizar questões</a>
-            <!--Botão para adicionar-->
-            <button name="inputSubmit" id="inputSubmit" type="submit" class="btn btn-success ml-1 mr-0">Adicionar</button>
-        </section>
-    </form>
-
-    <!--Seção de ajuda-->
-    <section>
-        <!--Ícone-->
-        <div class="d-flex justify-content-end mb-3 mr-3">
-            <img src="../../libraries/bootstrap/bootstrap-icons-1.0.0/question-circle-fill.svg" width="40" height="40" data-toggle="modal" data-target="#help" />
-        </div>
-
-        <!--Modal que exibe as orientações de ajuda-->
-        <div class="modal fade" id="help" tabindex="-1" role="dialog" aria-labelledby="helpModalCenterTitle" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="helpModalLongTitle">help</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
+    <form id="questionsForm" action="createSQL.php" method="post" data-toggle="validator">
+        <!--Seção principal-->
+        <section class="d-flex justify-content-center mt-3">
+            <div class="d-flex flex-column">
+                <!--Selects-->
+                <div class="d-flex flex-row mb-2">
+                    <!--Select das matérias-->
+                    <div class="w-25 mr-3">
+                        <label for="subjects" class="mt-1 mr-2">Matéria:</label>
+                        <select name="subjects" id="subjects" class="form-control" autofocus required>
+                            <option></option>
+                            <?php
+                            subjectNamesToDropdownItems($id_discipline);
+                            ?>
+                        </select>
                     </div>
-                    <div class="modal-body">
 
+                    <!--Select da dificuldade-->
+                    <div class="w-25 mr-3">
+                        <label for="dificulty" class="mt-1 mr-2">Dificuldade:</label>
+                        <select name="dificulty" id="dificulty" class="form-control" required>
+                            <option></option>
+                            <option value="1">Fácil</option>
+                            <option value="2">Médio</option>
+                            <option value="3">Difícil</option>
+                        </select>
                     </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-danger">Sair</button>
+
+                    <!--Select do número de alternativas-->
+                    <div class="w-25 mr-3">
+                        <label for="alternativesQuant" class="mt-1 mr-2">Nº de alternativas:</label>
+                        <select name="alternativesQuant" id="alternativesQuant" class="form-control" onchange="updateCorrectAnswerField_AlternativesField()" required>
+                            <option></option>
+                            <option value=4>4</option>
+                            <option value=5>5</option>
+                        </select>
+                    </div>
+
+                    <!--Select da alternativa correta-->
+                    <div class="w-25">
+                        <label for="correctAnswer" class="mt-1 mr-2">Alternativa correta:</label>
+                        <select name="correctAnswer" id="correctAnswer" class="form-control" disabled required></select>
                     </div>
                 </div>
+
+                <!--Enunciado da questão-->
+                <div>
+                    <!--Barra de ferramentas-->
+                    <div name="toolbar" id="toolbar-container"></div>
+                    <!--Campo de texto-->
+                    <div name="editor" id="editor" style="min-width: 65rem; max-width: 65rem;  min-height: 20rem; max-height: 20rem; border: 1px solid gray;" required></div>
+                </div>
+
+                <!--Enunciados das alternativas-->
+                <div class="d-flex justify-content-center">
+                    <div id="alternatives_container" class="d-flex flex-column mt-3"></div>
+                </div>
+
+                <!--Seção dos botões-->
+                <div class="d-flex flex-row justify-content-center mb-5">
+                    <!--Botão para cancelar-->
+                    <a href="../../views/home.php" type="button" class="w-25 btn btn-danger mr-2">Cancelar</a>
+                    <!--Botão para listar questões-->
+                    <a href="readGUI.php" type="button" class="w-50 btn btn-primary mr-2">Visualizar questões</a>
+                    <!--Botão para adicionar-->
+                    <button name="inputSubmit" id="inputSubmit" type="submit" class="w-25 btn btn-success">Adicionar</button>
+                </div>
             </div>
-        </div>
-    </section>
+        </section>
+    </form>
 
     <script>
         //Função para realizar a conexão CKEditor-MySQL.
@@ -161,6 +128,7 @@
             alternativesQuant = alternativesQuant.value;
 
             var selectCorrectAnswer = document.getElementById("correctAnswer");
+            selectCorrectAnswer.removeAttribute("disabled");
             selectCorrectAnswer.innerHTML = "";
 
             var option = document.createElement("option");
@@ -186,17 +154,18 @@
                 let img = document.createElement("img");
                 img.setAttribute("src", `../../images/alternatives/${alternatives[i]}.png`);
                 img.setAttribute("alt", alternatives[i]);
-                img.setAttribute("class", "rounded-circle mr-1 mb-3");
-                img.setAttribute("style", "background-color: powderblue;")
+                img.setAttribute("class", "bg-info rounded-circle mr-1 mb-3");
                 div.appendChild(img);
 
                 let textarea = document.createElement("textarea");
                 textarea.setAttribute("name", `question${i}`);
                 textarea.setAttribute("id", `question${i}`);
-                textarea.setAttribute("cols", "90");
+                textarea.setAttribute("cols", "120");
                 textarea.setAttribute("rows", "3");
                 textarea.setAttribute("class", "ml-1 mb-3");
                 textarea.setAttribute("style", "resize: none;");
+                textarea.setAttribute("placeholder", 'Insira o enunciado da alternativa...');
+                textarea.setAttribute("required", "true");
                 div.appendChild(textarea);
             }
         }
@@ -207,10 +176,12 @@
     <script src="../../libraries/bootstrap/bootstrap.bundle.js"></script>
 
     <!--Importação do CkEditor-->
-    <script src="https://cdn.ckeditor.com/ckeditor5/23.1.0/decoupled-document/ckeditor.js"></script>
+    <script src="../../libraries/ckeditor5/ckeditor.js"></script>
     <script>
         DecoupledEditor
-            .create(document.querySelector('#editor'))
+            .create(document.querySelector('#editor'), {
+                placeholder: 'Insira o enunciado da questão...'
+            })
             .then(editor => {
                 const toolbarContainer = document.querySelector('#toolbar-container');
 
