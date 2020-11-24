408a4a21f32b86a2ca3cb954b64af90c62646afc
jose-roberto <juserobertu@gmail.com>
2020-11-24 13:50:23 -0300

    planejamento entrega 3

---
+++ docs/entrega3.php
@@ -0,0 +1,33 @@
+function tasks(improvements, new_functionalitys) {
+    return improvements + " e " + new_functionalitys;
+}
+
+student jao, juse, nics;
+date_entrega3 = "08/12 - 11h30";
+improvements = "";
+new_functionalitys = "";
+
+if (jao.ITA("Aprovado!") {
+    date_entrega3 = "18/12 - 11h30";         
+}
+
+improvements = "cada questão sendo um link para seu respectivo modal";
+new_functionalitys = "crud_simpleTest";
+
+jao = tasks(improvements, new_functionalitys);
+
+improvements = "adicionar option 'Escolha...' no select disciplines e debug";
+new_functionalitys = "ud_question";
+
+juse = tasks(improvements, new_functionalitys);
+
+improvements = "";
+new_functionalitys = "d_login, cr_institution";
+
+nics = tasks(improvements, new_functionalitys);
+
+entrega3 = jao + nics + juse;
+
+if(datetime.system == data_entrega3) {
+    show(entrega3);
+} 
