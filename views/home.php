72d5892cebd1a713e46aacdc58cad7a0e8da774e
unknown <nigarofe@gmail.com>
2021-01-01 07:21:44 -0300

    p5

--- views/home.php
+++ views/home.php
@@ -10,6 +10,9 @@
     <link rel="stylesheet" href="/libraries/bootstrap/bootstrap.css">
     <script src="/libraries/bootstrap/jquery-3.5.1.js"></script>
     <script src="/libraries/bootstrap/bootstrap.bundle.js"></script>
+    <script src="/libraries/p5js/p5.js"></script>
+    <script src="/libraries/p5js/p5.dom.js"></script>
+    <script src="/libraries/p5js/p5.sound.js"></script>
 
     <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/utilities/sessionDebug.php'; ?>
 </head>
@@ -19,9 +22,57 @@
         <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/views/navbar.php'; ?>
     </header>
 
-    <main class="flex-grow-1" style="background-image: url(../images/background.jpeg);">
+    <script>
+        let increment = 0.01;
+        let start = 0;
 
-    </main>
+        function setup() {
+            createCanvas(1280, 700);
+            let canvas = document.getElementById("defaultCanvas0");
+            canvas.classList.add("flex-grow-1");
+            canvas.classList.add("w-100");
+
+
+            time = 0;
+            background(0);
+            time = 0;
+        }
+
+        function draw() {
+            background(0);
+            stroke(255);
+            strokeWeight(3);
+
+
+
+            textSize(100);
+            text("Autella", width/2 - 150, height/2);
+            // Comment line for interesting effect
+            //noFill();
+
+            beginShape();
+            let xOffset = start;
+            for (let i = 0; i < width; i++) {
+                let x = i;
+                let y = map(noise(xOffset), 0, 1, 0, height);
+                vertex(x, y);
+
+                xOffset += increment;
+            }
+            endShape();
+
+            start += increment;
+
+            // Diminui velocidade do gráfico
+            //start += increment / 8;
+
+            // Aumenta velocidade do gráfico
+            // start += increment * 8;
+
+        }
+    </script>
+
+
 </body>
 
 </html>
