57f47cd2393357bead0fcb98718cb0b767dd2ae3
unknown <nigarofe@gmail.com>
2021-01-02 17:14:59 -0300

    Permitir a coordenadores e gerenciadores do sistema Ativarem usuários

--- cruds/user/activateSQL.php
+++ cruds/user/activateSQL.php
@@ -1,28 +1,27 @@
 <?php
-if (isset($_POST['submit'])) {
-    require_once $_SERVER['DOCUMENT_ROOT'] . '/database/dbConnect.php';
-    require_once $_SERVER['DOCUMENT_ROOT'] . '/utilities/security.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/database/dbConnect.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/database/dbSelect/user.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/utilities/security.php';
 
-    $oldPassword = secure($_POST['oldPassword']);
+$oldPassword = secure($_POST['oldPassword']);
 
-    if ($oldPassword == $_SESSION['userData']['password']) {
-        date_default_timezone_set('America/Sao_Paulo');
+if ($oldPassword == $_SESSION['userData']['password'] || getAccountRole($_SESSION['userData']['id']) == 1 || getAccountRole($_SESSION['userData']['id']) == 5) {
+    date_default_timezone_set('America/Sao_Paulo');
 
-        $sql = "UPDATE user SET status='1' WHERE id=" . $_SESSION['userData']['id'];
+    $sql = "UPDATE user SET status='1' WHERE id=" . $_GET['id'];
 
-        if ($connection->query($sql) === TRUE) {
-            // array_push($_SESSION['debug'], "Conta ativada com sucesso!");
+    if ($connection->query($sql) === TRUE) {
+        // array_push($_SESSION['debug'], "Conta ativada com sucesso!");
 
-            // Logout
-            require_once $_SERVER['DOCUMENT_ROOT'] . '/authentication/logout.php';
-        } else {
-            array_push($_SESSION['debug'], "Erro ao ativar conta!");
-        }
+        // Logout
+        require_once $_SERVER['DOCUMENT_ROOT'] . '/authentication/logout.php';
     } else {
-        array_push($_SESSION['debug'], "Senha atual incorreta!");
+        array_push($_SESSION['debug'], "Erro ao ativar conta!");
     }
-
-    $connection->close();
-
-    header("Location: ../../index.php");
+} else {
+    array_push($_SESSION['debug'], "Senha atual incorreta!");
 }
+
+$connection->close();
+
+header("Location: ../../index.php");
