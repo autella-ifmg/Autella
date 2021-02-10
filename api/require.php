7aaf92d9c2c32751c433bc565cc8b8e382886f85
unknown <nigarofe@gmail.com>
2021-02-10 07:56:54 -0300

    Api - gabarito

--- api/require.php
+++ api/require.php
@@ -10,7 +10,7 @@ switch ($_GET['metodo']) {
             require_once $_SERVER['DOCUMENT_ROOT'] . '/autella.com/database/dbConnect.php';
             $sql = "SELECT name, id FROM tests";
             $result = mysqli_query($connection, $sql);
-            
+
             $resultado = '{
                             "provas":[
                                 
@@ -23,10 +23,35 @@ switch ($_GET['metodo']) {
                 $resultado .= '"id": ' . $row[1];
                 $resultado .= '},';
             }
-            
+
             $resultado .= "]}";
             $resultado = str_replace(',]', ']', $resultado);
 
             echo $resultado;
+            break;
+        }
+        case 2: {
+            // require_once $_SERVER['DOCUMENT_ROOT'] . '/database/dbConnect.php';
+            require_once $_SERVER['DOCUMENT_ROOT'] . '/autella.com/database/dbConnect.php';
+            $sql = "SELECT question.correctAnswer FROM question JOIN question_test ON question.id = question_test.id_question JOIN tests ON tests.id = question_test.id_tests WHERE tests.id = " . $_GET['idDaProva'];
+            $result = mysqli_query($connection, $sql);
+
+            $resultado = '{
+                            "gabarito":[
+                                
+                            
+                ';
+
+            while ($row = mysqli_fetch_row($result)) {
+                $resultado .= '{';
+                $resultado .= '"alternativaCorreta": "' . $row[0] . '"';
+                $resultado .= '},';
+            }
+
+            $resultado .= "]}";
+            $resultado = str_replace(',]', ']', $resultado);
+
+            echo $resultado;
+            break;
         }
 }
