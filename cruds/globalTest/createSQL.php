1fa3a2950ef510d2dac2f261dc13f81b0c9fc49e
jose-roberto <juserobertu@gmail.com>
2021-02-10 21:35:01 -0300

    GlobaTest - createGUI: restrição

--- cruds/globalTest/createSQL.php
+++ cruds/globalTest/createSQL.php
@@ -20,73 +20,71 @@ function dateTratamentChange($date)
     return $date = "Ultima modificação em: " . date("d/m/Y", $date);
 }
 function data()
-{   
+{
     $array = SimpleTestes();
     echo '    <div class="split left " style="width:44%;right:0%;left:2%"> <table class="table"> <thead class="thead-dark"> <tr> <th style="width:20%;"> NOME DA PROVA </th> <th style="width:20%;"> DATA EM QUE FOI FEITA </th> <th style="width:20%;"> DATA DE ULTIMA MODIFICAÇÃO </th> <th style="width:20%;"> PROFESSOR QUE CRIOU </th> <th style="width:20%;"> SELECIONAR PARA PROVA GLOBAL </th> </thead>';
     //var_dump($array);
     if (!empty($array)) {
-       
         if (count($array) > 0) {
             for ($i = 0; $i < count($array); $i++) {
-                if($array[$i][5] != -1){
-                $datamaking = "".dateTratament($array[$i][2]);
-                $datachanging = "".dateTratamentChange($array[$i][3]);
-                $nameTest = $array[$i][4];
-                $nameTeacher = $array[$i][6];
-                $id_test = $array[$i][0];
-                echo '<table class ="table" id = "simpleTest'.$id_test.'"> 
+                if ($array[$i][5] != -1 && $array[$i][6] == 0) {
+                    $datamaking = "" . dateTratament($array[$i][2]);
+                    $datachanging = "" . dateTratamentChange($array[$i][3]);
+                    $nameTest = $array[$i][4];
+                    $nameTeacher = $array[$i][6];
+                    $id_test = $array[$i][0];
+                    echo '<table class ="table" id = "simpleTest' . $id_test . '"> 
                 <div>
                 <tr>
-                <td style="width:20%;" id = "name'.$id_test.'">  <a href="http://autella.com/cruds/simpleTest/readTestGUI.php?id='.$id_test.'">'.$nameTest.' </a></td>
-                <td style="width:20%;" id = "dataMaking'.$id_test.'">'.$datamaking.' </td>
-                <td style="width:20%;" id = "dataChanging'.$id_test.'">'.$datachanging.' </td>
-                <td style="width:20%;" id = "nameTeacher'.$id_test.'">'.$nameTeacher.' </td>
+                <td style="width:20%;" id = "name' . $id_test . '">  <a href="http://autella.com/cruds/simpleTest/readTestGUI.php?id=' . $id_test . '">' . $nameTest . ' </a></td>
+                <td style="width:20%;" id = "dataMaking' . $id_test . '">' . $datamaking . ' </td>
+                <td style="width:20%;" id = "dataChanging' . $id_test . '">' . $datachanging . ' </td>
+                <td style="width:20%;" id = "nameTeacher' . $id_test . '">' . $nameTeacher . ' </td>
                 
                 <td>
-                  <img  src=../../../libraries/bootstrap/bootstrap-icons-1.0.0/arrow-right-square.svg alt=Editar height=25 onclick= "globalSend('.$id_test.')"/> 
+                  <img  src=../../../libraries/bootstrap/bootstrap-icons-1.0.0/arrow-right-square.svg alt=Editar height=25 onclick= "globalSend(' . $id_test . ')"/> 
                 </td>
                 </tr>
                 </div></table>';
-            }
+                }
             }
         }
-       
     }
     echo '</div></table>';
-    
 }
 
-    function deletTest($id_test){
-        date_default_timezone_set("America/Sao_Paulo");
-        $date = date("Y-m-d");
-        //echo $id_test;
-        global $connection;
-   
-        
-        $sql = "UPDATE autella.tests set status = -1 WHERE id = '$id_test';";
-        echo $sql;
-        $connection->query($sql);
-    }
+function deletTest($id_test)
+{
+    date_default_timezone_set("America/Sao_Paulo");
+    $date = date("Y-m-d");
+    //echo $id_test;
+    global $connection;
 
 
-    function insertInDatabase($globalList,$GlobalName)
+    $sql = "UPDATE autella.tests set status = -1 WHERE id = '$id_test';";
+    echo $sql;
+    $connection->query($sql);
+}
+
+
+function insertInDatabase($globalList, $GlobalName)
 {
     date_default_timezone_set("America/Sao_Paulo");
     $date = date("Y-m-d");
     //var_dump($globalList);
     global $connection;
     $id_user = $_SESSION["userData"]["id"];
-    $sql = "INSERT into Global(id_user, making_date, changing_date, name) VALUES ('$id_user','$date','$date','$GlobalName');";  
-   mysqli_query($connection, $sql);
+    $sql = "INSERT into Global(id_user, making_date, changing_date, name) VALUES ('$id_user','$date','$date','$GlobalName');";
+    mysqli_query($connection, $sql);
     $id_global =  mysqli_insert_id($connection);
     if (!empty($globalList)) {
         if (count($globalList) > 0) {
             for ($i = 0; $i != count($globalList); $i++) {
-                $id_test = $globalList[$i];    
+                $id_test = $globalList[$i];
                 $sql = "INSERT into test_Global(id_global, id_tests) VALUES ('$id_global','$id_test');";
                 //echo $sql;
-               mysqli_query($connection, $sql);
+                mysqli_query($connection, $sql);
             }
-        }       
+        }
     }
 }
