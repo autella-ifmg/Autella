8df50d09aa3b9e90439f7a7b0dbfc9c4cd78cb48
unknown <nigarofe@gmail.com>
2021-03-01 16:28:17 -0300

    NAwofnóawifawçoawofiow milagre

--- app/app/src/main/java/com/example/autella/MainActivity.java
+++ app/app/src/main/java/com/example/autella/MainActivity.java
@@ -1,11 +1,22 @@
 package com.example.autella;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import static android.content.ContentValues.TAG;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
         botaoVisualizarGabaritos = (Button) findViewById(R.id.botaoVisualizarGabaritos);
 
         cadastrarEventos();
+
+        FirebaseMessaging.getInstance().subscribeToTopic("all");
     }
 
     private void cadastrarEventos() {
@@ -31,4 +44,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
 }
