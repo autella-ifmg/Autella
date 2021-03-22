46f46e93052c33bddf02d110f901b1028cf8e38b
jose-roberto <juserobertu@gmail.com>
2021-03-22 19:09:46 -0300

    Bugfix: Erro ao criar questão após alternância de instituição.

--- controlPanel/allInstitutionsReadSQL.php
+++ controlPanel/allInstitutionsReadSQL.php
@@ -1,11 +1,20 @@
 <?php
-if (isset($_POST["id_institution"])) {
-    session_start();
+if (isset($_POST['id_institution'])) {
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/database/dbConnect.php';
 
-    $id_institution = $_POST["id_institution"];
+    $id_user = $_SESSION['userData']['id'];
+    $id_institution = $_POST['id_institution'];
 
-    $_SESSION["userData"][6] = $id_institution;
-    $_SESSION["userData"]["id_institution"] = $id_institution;
+    $_SESSION['userData'][6] = $id_institution;
+    $_SESSION['userData']["id_institution"] = $id_institution;
 
-    //array_push($_SESSION['debug'], "Instituição selecionada!");
+    $sql = "UPDATE user SET id_institution = '$id_institution' WHERE id = '$id_user'";
+
+    if ($connection->query($sql) === TRUE) {
+        //array_push($_SESSION['debug'], "Nova instituição selecionada!");
+    } else {
+        //array_push($_SESSION['debug'], "Erro ao selecionar nova instituição selecionada!");
+    }
+
+    $connection->close();
 }
